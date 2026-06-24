cd042891f283cc5be7d78b9f29d5b2a64e2ba876
Posty78 <141333715+Posty78@users.noreply.github.com>
2026-06-24 23:36:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+```javascript
+import { db } from "./firebase.js";
 
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const map = L.map("map").setView([46.6,2.2],6);
+
+L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        maxZoom:19,
+        attribution:"© OpenStreetMap"
+    }
+).addTo(map);
+
+const snap = await getDoc(doc(db,"project","status"));
+
+if(snap.exists()){
+
+    const current = snap.data().currentMcdo;
+
+    document.getElementById("progress").innerHTML =
+        current +
+        " / 1500<br>" +
+        ((current/1500)*100).toFixed(1) +
+        " %";
+
+}
+```
